14f3df1e90192d74fed3d8ad63cda932242e9f5d
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-07-16 15:20:49 +0000

    Feat: Signup controller

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
+const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   email: { 
     type: String,
-    required: true,
+    required: [true, 'Please enter your email id'],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
    },
   password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, 'Please enter an password'],
+    minlength: [6, 'Passwords must have atleast 6 characters in length']
   }
+})
+
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 })
 
 const User = mongoose.model('User', UserSchema);
